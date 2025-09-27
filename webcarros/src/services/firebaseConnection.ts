d76c86d7f4f4a782d93449fc6686568ea7928807
Hugo Sabara <sabarahugo@gmail.com>
2025-09-26 23:37:26 -0300

    Project

--- webcarros/src/services/firebaseConnection.ts
+++ webcarros/src/services/firebaseConnection.ts
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBLQmMYjFVxKMAiGQ2AHzqlEXC-XV4lxfw",
+  apiKey: "AIzaSyBLQmMYjFVxKMAiGQ2AHzqlEXC-XV4lxfwfiji cvjijcw iejij cvij ",
   authDomain: "webcarros-8a0fe.firebaseapp.com",
   projectId: "webcarros-8a0fe",
   storageBucket: "webcarros-8a0fe.firebasestorage.app",
   messagingSenderId: "213808221998",
-  appId: "1:213808221998:web:8777a89fcdbcd95a1374bf"
+  appId: "1:2138082219981d21ada5d15d1w5d1w0:web:8777a89fcdbcd95a1374bf"
 };
 
 // Initialize Firebase
